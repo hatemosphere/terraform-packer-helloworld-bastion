48d2d50d694c3d6209998ef214471684873d6a97
hatemosphere <hatemosphere@protonmail.com>
2019-07-17 21:34:18 +0300

    Adding dump CloudWatch alert on host down, local types regression fix

---
+++ terraform/providers/aws/us-east-1/infra/compute/bastion/elb_alert.tf
@@ -0,0 +1,32 @@
+resource "aws_cloudwatch_metric_alarm" "bastion-ssh-elb-alert" {
+  alarm_name          = "Bastion SSH ELB has unhealthy host"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+
+  period              = "60"
+  evaluation_periods  = "1"
+  datapoints_to_alarm = 1
+
+  # second
+  statistic         = "Maximum"
+  threshold         = "1"
+  alarm_description = ""
+
+  metric_name = "UnHealthyHostCount"
+  namespace   = "AWS/ELB"
+  dimensions = {
+    LoadBalancerName = module.bastion_elb.this_elb_name
+  }
+
+  actions_enabled           = true
+  insufficient_data_actions = []
+  ok_actions                = []
+  alarm_actions             = [module.sns-email-topic.arn]
+}
+
+module "sns-email-topic" {
+  source  = "github.com/koalificationio/tf-sns-email-list?ref=0.0.1"
+
+  display_name    = "CloudWatch Bastion SSH ELB Alerts"
+  email_addresses = ["admin@example.com"]
+  stack_name      = "bastion-ssh-alerts-sns-stack"
+}
--- terraform/providers/aws/us-east-1/infra/compute/bastion/locals.tf
+++ terraform/providers/aws/us-east-1/infra/compute/bastion/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  ssh_keypair_name    = "xxx"
-  vpc_id              = [data.terraform_remote_state.infra_vpc.outputs.vpc_id]
-  public_subnets  = [data.terraform_remote_state.infra_vpc.outputs.public_subnets]
-  private_subnets = [data.terraform_remote_state.infra_vpc.outputs.private_subnets]
+  ssh_keypair_name = "dreamteam-infra"
+  vpc_id           = data.terraform_remote_state.infra_vpc.outputs.vpc_id
+  public_subnets   = data.terraform_remote_state.infra_vpc.outputs.public_subnets
+  private_subnets  = data.terraform_remote_state.infra_vpc.outputs.private_subnets
 }
